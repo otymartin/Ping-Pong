8aaaffe97886fa919119065b41db035ce3037008
Martin Otyeka <otyeka.martin@gmail.com>
2022-06-08 16:18:33 -0400

    Initial Commit

---
+++ Ping Pong/Ping_PongApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ping_PongApp.swift
+//  Ping Pong
+//
+//  Created by Martin Otyeka on 2022-06-08.
+//
+
+import SwiftUI
+
+@main
+struct Ping_PongApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
